6522ce96aaaa54f4b99bbd2f49140f982e4f222a
akashrajum7 <akashrajum7@gmail.com>
2020-09-04 09:45:53 +0530

    Setup postgresql and migrations

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: "lireddit",
-    type: "postgresql",
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+  const post = orm.em.create(Post, { title: "My first post" });
+  await orm.em.persistAndFlush(post);
 };
 
-main();
+main().catch((err) => console.log(err));
